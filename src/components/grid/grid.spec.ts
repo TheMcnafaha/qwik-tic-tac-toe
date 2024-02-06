be0620372ee23763af145821a0ffed04363e8c18
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-02-05 19:43:19 -0500

    simple algo

--- src/components/grid/grid.spec.ts
+++ src/components/grid/grid.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { mazefier, testlol } from "./utils";
+import solve, { mazefier, testlol } from "./utils";
 import { Grid2dArr } from "./grid";
 
 test("adds 1 + 2 to equal 3", () => {
@@ -9,6 +9,10 @@ test("adds 1 + 2 to equal 3", () => {
 const ex1Grid = [[{value:"X"}, {value:"X"}, {value:"X"}]];
 
 test("work algo", ()=>{
-  mazefier(ex1Grid,"X")
+  // mazefier(ex1Grid,"X")
   // expect( mazefier(ex1Grid)).toBe(2)
+  const lol=solve(ex1Grid as Grid2dArr,"O",{x:0,y:0})
+  console.log(lol);
+  expect(lol.length).toBe(3)
+  
 })
--- src/components/grid/utils.ts
+++ src/components/grid/utils.ts
@@ -19,24 +19,22 @@ export function find3() {
   return arr.length;
 }
 export default function solve(
-  maze: string[],
+  maze: Grid2dArr,
   wall: string,
   start: Point,
-  end: Point,
 ): Point[] {
   const seen: boolean[][] = [];
   const path: Point[] = [];
   for (let index = 0; index < maze.length; index++) {
     seen.push(new Array(maze[0].length).fill(false));
   }
-  walk(maze, wall, start, end, seen, path);
+  walk(maze, wall, start, seen, path);
   return path;
 }
 function walk(
-  maze: string[],
+  maze: Grid2dArr,
   wall: string,
   curr: Point,
-  end: Point,
   seen: boolean[][],
   path: Point[],
 ): boolean {
@@ -47,11 +45,11 @@ function walk(
     return false;
   }
 
-  if (maze[curr.y][curr.x] === wall) {
+  if (maze[curr.y][curr.x].value === wall) {
     return false;
   }
-  if (curr.x === end.x && curr.y === end.y) {
-    path.push(end);
+  if (path.length === 3) {
+    console.log("ending");
     return true;
   }
   if (seen[curr.y][curr.x]) {
@@ -62,38 +60,10 @@ function walk(
   for (let index = 0; index < dir.length; index++) {
     const [x, y] = dir[index];
     const newPoint = { x: curr.x + x, y: curr.y + y };
-    if (walk(maze, wall, newPoint, end, seen, path)) {
+    if (walk(maze, wall, newPoint, seen, path)) {
       return true;
     }
   }
   path.pop();
   return false;
-}
-
-export function mazefier(grid: Grid2dArr, strg: "X" | "O") {
-  const maze = [];
-  let output = "";
-  for (let index = 0; index < grid.length; index++) {
-    console.log(output);
-    const row = grid[index];
-    if (index === 0) {
-      output = padder(row);
-      maze.push(output);
-    }
-  }
-
-  console.log(maze);
-}
-function rowfier(row: Grid1DArr, strg: "X" | "O", cIndex: number) {}
-
-function padder(row: Grid1DArr) {
-  let output = "";
-  for (let index = 0; index < row.length; index++) {
-    output += "#";
-  }
-  return addEnds(output);
-}
-
-function addEnds(strg: string) {
-  return "#".concat(strg, "#");
 }
