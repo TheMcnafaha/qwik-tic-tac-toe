b0564b47872c793070af9aa01b6c007fb271d4b3
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-02-06 21:12:22 -0500

    working diagonal

--- src/components/grid/grid.spec.ts
+++ src/components/grid/grid.spec.ts
@@ -45,5 +45,15 @@ const ex4Grid= [
   const lol4=solve(ex4Grid as Grid2dArr,"O","X",{x:0,y:0})
   console.log(lol4);
   expect(lol4.length).toBe(3)
-  
-})
+  })
+
+test.only("work algo", ()=>{
+const ex5Grid= [
+  [{value:"X"}, {value:"O"}, {value:"O"}],
+  [{value:"O"}, {value:"X"}, {value:"X"}],
+  [{value:"O"}, {value:"O"}, {value:"X"}],
+];
+  const lol5=solve(ex5Grid as Grid2dArr,"O","X",{x:0,y:0})
+  console.log(lol5);
+  expect(lol5.length).toBe(3)
+  })
--- src/components/grid/utils.ts
+++ src/components/grid/utils.ts
@@ -1,5 +1,5 @@
 import { SquareValues } from "../square/square";
-import { Grid2dArr } from "./grid";
+import { type Grid2dArr } from "./grid";
 
 type Point = {
   x: number;
@@ -13,11 +13,15 @@ export function testlol() {
   return "working";
 }
 
-const dir = [
+const dirOrth = [
   [0, 1],
   [0, -1],
   [1, 0],
   [-1, 0],
+];
+const dirDiago = [
+  [1, 1],
+  [-1, -1],
 ];
 export function find3() {
   const arr: string[] = [];
@@ -37,10 +41,11 @@ export default function solve(
     seen.push(new Array(maze[0].length).fill(false));
     seenOnce.push(new Array(maze[0].length).fill(false));
   }
-  walk(maze, wall, key, start, seen, path, lol);
+  walkDiago(maze, wall, key, start, seen, path, lol);
   return lol;
 }
-function walk(
+
+function walkDiago(
   maze: Grid2dArr,
   wall: string,
   key: SquareValues,
@@ -84,22 +89,78 @@ function walk(
   }
   path.push(curr);
   seen[curr.y][curr.x] = true;
-  for (let index = 0; index < dir.length; index++) {
-    const [x, y] = dir[index];
+  for (let index = 0; index < dirDiago.length; index++) {
+    const [x, y] = dirDiago[index];
     const newPoint = { x: curr.x + x, y: curr.y + y };
-    if (walk(maze, wall, key, newPoint, seen, path, values, index)) {
+    if (walkDiago(maze, wall, key, newPoint, seen, path, values, index)) {
       return true;
     }
   }
   path.pop();
   return false;
 }
+function walkOrtho(
+  maze: Grid2dArr,
+  wall: string,
+  key: SquareValues,
+  curr: Point,
+  seen: boolean[][],
+  path: Point[],
+  values: ValueProps,
+  idx: number = -1,
+): boolean {
+  const offXAxis = curr.x < 0 || curr.x >= maze[0].length;
+  const offYAxis = curr.y < 0 || curr.y >= maze.length;
+  if (offXAxis || offYAxis) {
+    return false;
+  }
 
+  const entry = maze[curr.y][curr.x];
+  if (maze[curr.y][curr.x].value === wall) {
+    return false;
+  }
+  if (values.length === 3) {
+    return true;
+  }
+  if (seen[curr.y][curr.x]) {
+    return false;
+  }
+
+  if (entry.value === key) {
+    const last = values.at(-1);
+    if (values.length > 0 && idx !== -1 && last.dir !== -1) {
+      if (!isPair(idx, last?.dir)) {
+        console.log("new boi ", last.dir, idx);
+        // this only works because arr is size 3,
+        // the dynamic solution is to make the arr equal the last two elems,
+        // which for a size 3 arr is always the case with shift
+        values.shift();
+      }
+    }
+
+    const obj = { point: curr, dir: idx };
+    values.push(obj);
+  }
+  path.push(curr);
+  seen[curr.y][curr.x] = true;
+  for (let index = 0; index < dirOrth.length; index++) {
+    const [x, y] = dirOrth[index];
+    const newPoint = { x: curr.x + x, y: curr.y + y };
+    if (walkOrtho(maze, wall, key, newPoint, seen, path, values, index)) {
+      return true;
+    }
+  }
+  path.pop();
+  return false;
+}
 function isPair(idx: number, prevIdx) {
   if (idx === 0 || idx === 1) {
     return prevIdx === 0 || prevIdx === 1;
   }
   if (idx === 2 || idx === 3) {
     return prevIdx === 2 || prevIdx === 3;
+  }
+  if (idx === 4 || idx === 5) {
+    return prevIdx === 4 || prevIdx === 5;
   }
 }
